088ed74247565175e83c8ec9c22b619c82d47d3c
Marco LUCARELLA <lucarellamarco@yahoo.it>
2021-09-18 20:51:23 +0200

    Create working communicaiton between joplin and markdown view

--- src/contentScript/contentScript.ts
+++ src/contentScript/contentScript.ts
@@ -1,14 +1,17 @@
+// import MarkdownIt = require("markdown-it")
+import * as MarkdownIt from "markdown-it"
 const tokenName = "jira"
 
 export default function (context) {
     return {
-        plugin: function (markdownIt, _options) {
+        plugin: function (markdownIt: MarkdownIt, _options) {
             const contentScriptId = context.contentScriptId;
+            // const pluginId = context.pluginId;
             console.log("context", context)
             let divIndex = 0
 
             const defaultRender = markdownIt.renderer.rules.fence || function (tokens, idx, options, env, self) {
-                return self.renderToken(tokens, idx, options, env, self)
+                return self.renderToken(tokens, idx, options)
             }
 
             markdownIt.renderer.rules.fence = function (tokens, idx, options, env, self) {
@@ -16,32 +19,34 @@ export default function (context) {
                 if (token.info !== tokenName) return defaultRender(tokens, idx, options, env, self)
 
                 divIndex++
-                const content = token.content.trim().replace(/\n/g, ';')
+                const content = token.content.trim().replace(/\n/g, ';').replace(/'/g, "\'")
+
+                const sendContentToJoplinPlugin = `
+                    console.log('reload', ${divIndex});
+                    webviewApi.postMessage('${contentScriptId}', '${content}').then((response) => {
+                        console.info('Response-${divIndex}: ' + response);
+                    });
+                `.replace(/\n/g, ' ').replace(/"/g, '\\"')
+                // document.getElementById("jira-issue-root-${divIndex}").innerHTML = response
+
                 return `
-                <div id="jira-issue-root-${divIndex}">
+                <div id="jira-issue-root-${divIndex}" onReady="alert(${divIndex})">
                     <div class="jira-container">
                         <div class="jira-issue flex-center">
                             <div class="lds-dual-ring"></div>
-                            <span>-</span>
+                            <span id="id1">-</span>
                             <span>Getting issue details...</span>
                             <span class="tag tag-grey outline" title="Status">STATUS</span>
                         </div>
                     </div>
                 </div>
-                <script>
-                    console.log("reload", ${divIndex})
-                    webviewApi.postMessage('${contentScriptId}', '${content}').then(function(response) {
-                        console.log("jira-issue-root-${divIndex}", response)
-                        // document.getElementById("jira-issue-root-${divIndex}").innerHTML = response
-                    })
-                </script>
+                <style onload="${sendContentToJoplinPlugin}"></style>
                 `
             }
         },
         assets: function () {
             return [
                 { name: 'style.css' },
-                // { name: 'markdownItPluginController.js' },
             ]
         },
     }
