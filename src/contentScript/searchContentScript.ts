9b9c2cf9427ee160946522f6d79f7a23070cc6e2
Marco LUCARELLA <lucarellamarco@yahoo.it>
2021-09-24 00:06:46 +0200

    Support jirasearch html tag

--- src/contentScript/searchContentScript.ts
+++ src/contentScript/searchContentScript.ts
@@ -1,6 +1,19 @@
 import * as MarkdownIt from "markdown-it"
 const tokenName = "jira-search"
 
+function unpackAttributes(attributesStr: string): any {
+    const attributesObj = {}
+    while (attributesStr.length > 0) {
+        const matches = attributesStr.match(/ *(?<key>[a-z]+)=\"(?<value>[^"]+)\" */)
+        if (!matches || !matches.groups) {
+            break
+        }
+        attributesObj[matches.groups.key] = matches.groups.value.replace(/&quot/g, '"')
+        attributesStr = attributesStr.slice(matches[0].length)
+    }
+    return attributesObj
+}
+
 export default function (context) {
     return {
         plugin: function (markdownIt: MarkdownIt, _options) {
@@ -30,6 +43,41 @@ export default function (context) {
 
                 return `
                 <div id="jira-search-root-${renderIndex}" class="jira-container container-block">
+                    <div class="jira-issue flex-center">
+                        <div class="lds-dual-ring"></div>
+                        <span>-</span>
+                        <span>Getting search results...</span>
+                        <span class="tag tag-grey outline" title="Status">STATUS</span>
+                    </div>
+                </div>
+                <style onload="${sendContentToJoplinPlugin}"></style>
+                `
+            }
+
+            const defaultHtmlInlineRender = markdownIt.renderer.rules.html_inline || function (tokens, idx, options, env, self) {
+                return self.renderToken(tokens, idx, options)
+            }
+            markdownIt.renderer.rules.html_inline = function (tokens, idx, options, env, self) {
+                const token = tokens[idx]
+                console.log('html_inline', idx, tokens)
+                const matches = token.content.toLowerCase().match(/<jirasearch +(?<attributes>[^>]+?) *\/?>/)
+                if (!matches || !matches.groups) return defaultHtmlInlineRender(tokens, idx, options, env, self)
+                renderIndex++
+                console.log(`Jira-Search[${renderIndex}] render markdown-it plugin html_inline`)
+
+                const attributes = unpackAttributes(matches.groups.attributes)
+
+                const content = JSON.stringify(attributes.jql)
+
+                const sendContentToJoplinPlugin = `
+                    console.log('Jira-Search[${renderIndex}] send content:', ${content});
+                    webviewApi.postMessage('${contentScriptId}', ${content}).then((response) => {
+                        document.getElementById('jira-search-root-${renderIndex}').innerHTML = response;
+                    });
+                `.replace(/"/g, '&quot;')
+
+                return `
+                <div id="jira-search-root-${renderIndex}" class="jira-container container-inline">
                     <div class="jira-issue flex-center">
                         <div class="lds-dual-ring"></div>
                         <span>-</span>
